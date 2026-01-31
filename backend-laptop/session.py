641cf405b4a2315acac0e662c045b3459910895c
dmeyer4569 <dmeyer2@oakland.edu>
2026-01-31 16:52:28 -0500

    session.py is ready

---
+++ backend-laptop/session.py
@@ -0,0 +1,21 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+# sql alchemy engine
+def get_engine(testing: bool = False):
+    engine = create_async_engine("sqlite+aiosqlite:///db.sqlite", echo=True)
+
+    return engine
+
+engine = get_engine()
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
