61066eafe4665dcf4afe93292f0884ce188b0867
dmeyer4569 <dmeyer2@oakland.edu>
2026-01-31 21:43:56 -0500

    static img folder

--- backend-laptop/main.py
+++ backend-laptop/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.staticfiles import StaticFiles
 from session import init_db, get_db
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,8 +13,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(main_router, prefix="/api") # everything? Not decided as of rn
 app.include_router(filler_router, prefix="/testing") # fill the db for testing purposes
 
-
-print("Baller")
